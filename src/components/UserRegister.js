3dff4304b896b12cc7e8cb6218445fae2e039d75
Mahabubhasan01 <mahabubhasaan@outlook.com>
2023-07-02 23:00:07 +0600

    encryted added

--- src/components/UserRegister.js
+++ src/components/UserRegister.js
@@ -37,17 +37,110 @@ const UserRegister = () => {
         body: JSON.stringify(formData),
       });
 
-      if (response) {
+      if (response.ok) {
         const data = await response.json();
         toast.success("Successfully registered");
         // Setting user information in local storage
-        const userInfo = {
-          username: username,
+        const userData = {
+          name: username,
           email: email,
         };
-        localStorage.setItem("userInfo", JSON.stringify(userInfo));
-        navigate("/chat");
+        localStorage.setItem("userInfo", JSON.stringify(userData));
 
+        /* encrypted data */
+
+        // Generate a random initialization vector (IV)
+        function generateIV() {
+          return crypto.getRandomValues(new Uint8Array(16));
+        }
+
+        // Encrypt user data and store it in local storage
+        function encryptAndStoreData(userData, encryptionKey) {
+          // Convert the user data to a string
+          const stringifiedUserData = JSON.stringify(userData);
+
+          // Generate an IV
+          const iv = generateIV();
+
+          // Convert the data to an ArrayBuffer
+          const dataBuffer = new TextEncoder().encode(stringifiedUserData);
+
+          // Encrypt the data using the encryption key and IV
+          crypto.subtle
+            .encrypt({ name: "AES-CBC", iv }, encryptionKey, dataBuffer)
+            .then((encryptedData) => {
+              // Store the encrypted data and IV in local storage
+              localStorage.setItem(
+                "encryptedData",
+                new Uint8Array(encryptedData)
+              );
+              localStorage.setItem("iv", new Uint8Array(iv));
+              console.log("User data encrypted and stored in local storage");
+            })
+            .catch((error) => {
+              console.error("Error encrypting and storing user data:", error);
+            });
+        }
+
+        // Decrypt user data from local storage
+        function decryptStoredData(encryptionKey) {
+          // Retrieve the encrypted data and IV from local storage
+          const encryptedData = localStorage.getItem("encryptedData");
+          const iv = localStorage.getItem("iv");
+
+          // Convert the data back to Uint8Arrays
+          const encryptedDataArray = new Uint8Array(encryptedData);
+          const ivArray = new Uint8Array(iv);
+
+          // Decrypt the data using the encryption key and IV
+          crypto.subtle
+            .decrypt(
+              { name: "AES-CBC", iv: ivArray },
+              encryptionKey,
+              encryptedDataArray
+            )
+            .then((decryptedData) => {
+              // Convert the decrypted data to a string
+              const decryptedString = new TextDecoder().decode(decryptedData);
+
+              // Parse the decrypted user data
+              const decryptedUserData = JSON.parse(decryptedString);
+
+              console.log("Decrypted user data:", decryptedUserData);
+            })
+            .catch((error) => {
+              console.error("Error decrypting stored user data:", error);
+            });
+        }
+
+        // Generate a cryptographic key for encryption/decryption
+        crypto.subtle
+          .generateKey({ name: "AES-CBC", length: 256 }, true, [
+            "encrypt",
+            "decrypt",
+          ])
+          .then((key) => {
+            // Store the encryption key for later use
+            const encryptionKey = key;
+
+            // Example user data
+            /* const userData = {
+              id: 1,
+              name: "John Doe",
+              email: "johndoe@example.com",
+            }; */
+
+            // Encrypt and store the user data
+            encryptAndStoreData(userData, encryptionKey);
+
+            // Decrypt the stored data
+            decryptStoredData(encryptionKey);
+          })
+          .catch((error) => {
+            console.error("Error generating encryption key:", error);
+          });
+        navigate("/chat");
+        /* encrypted data */
         console.log(data);
         // Perform any necessary actions after successful registration
       } else {
--- src/utils/ToggleImageWithMenu.js
+++ src/utils/ToggleImageWithMenu.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/ToggleImageWithMenu.css";
+import { useNavigate } from "react-router-dom";
+
 const ToggleImageWithMenu = ({ userDetail }) => {
+  const history = useNavigate();
+  const handleLogout = () => {
+    // Perform logout actions here
+    // e.g., clear session data, remove tokens from local storage
+
+    // Redirect to the login page
+    history("/");
+  };
   return (
     <div class="menu-wrap">
       <ul class="menu">
@@ -19,7 +29,7 @@ const ToggleImageWithMenu = ({ userDetail }) => {
               <Link href="#">Settings</Link>
             </li>
             <li class="drop-menu-item">
-              <Link href="#">Logout</Link>
+              <Link onClick={handleLogout}>Logout</Link>
             </li>
           </ul>
         </li>
